6a3973e0b7bf6c276e5f675b572497b8e18d629c
nam141001 <lehoangnam20011410@gmail.com>
2025-05-02 10:27:51 +0700

    update controller

---
+++ src/main/java/com/example/AccountService/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.example.AccountService.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.AccountService.model.Account;
+
+
+public interface AccountRepository extends JpaRepository<Account, Long>{
+
+}
