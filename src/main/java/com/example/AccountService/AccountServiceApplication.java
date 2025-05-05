a2d8dc10fcdcd1256fcf49d3e0432d407b729698
nam141001 <lehoangnam20011410@gmail.com>
2025-05-05 19:55:45 +0700

    update

--- src/main/java/com/example/AccountService/AccountServiceApplication.java
+++ src/main/java/com/example/AccountService/AccountServiceApplication.java
@@ -1,7 +1,10 @@
 package com.example.AccountService;
-
+import org.modelmapper.config.Configuration;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccountServiceApplication {
@@ -9,5 +12,12 @@ public class AccountServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountServiceApplication.class, args);
 	}
-
+	@Bean
+    public ModelMapper modelMapper() {
+	ModelMapper modelMapper = new ModelMapper();
+	modelMapper.getConfiguration().setFieldMatchingEnabled(true)
+		.setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+		.setMatchingStrategy(MatchingStrategies.STRICT);
+	return modelMapper;
+    }
 }
